7bac3e2d7a48759c6bc2ad1736830342361fa256
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2024-05-25 17:43:20 +0200

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -20,6 +20,14 @@ public class Item : MonoBehaviour
     }
 }
 
+import pygame
+import time
+pygame.init()
+'''creating the program window''
+back = (200, 255, 255) ##background color
+mw = pygame.display.set_mode((500, 500)) #main window
+mw.fill(back)
+clock = pygame.time.Clock()
 
 class Area():
   def __init__(self, x=0, y=0, width=10, height=10, color=None):
