18931aeb0c49ff1f0a00e0ae65779177293dfd44
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2024-05-25 17:38:32 +0200

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -21,3 +21,23 @@ public class Item : MonoBehaviour
 }
 
 
+class Area():
+  def __init__(self, x=0, y=0, width=10, height=10, color=None):
+      self.rect = pygame.Rect(x, y, width, height) 
+      self.fill_color = color
+  def color(self, new_color):
+      self.fill_color = new_color
+  def fill(self):
+      pygame.draw.rect(mw, self.fill_color, self.rect)
+  def outline(self, frame_color, thickness):  #outline of an existing rectangle
+      pygame.draw.rect(mw, frame_color, self.rect, thickness)   
+  def collidepoint(self, x, y):
+      return self.rect.collidepoint(x, y)      
+'''Class label'''
+class Label(Area):
+  def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+      self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+  def draw(self, shift_x=0, shift_y=0):
+      self.fill()
+      mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
